b2e2fa1e40e4554dd5e9fc0c13973149584c4775
hendryvwendel <hendryvwendel@gmail.com>
2024-09-18 08:34:20 +0200

    Updated

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SourcePath = new System.Windows.Forms.TextBox();
+            this.SelectImages = new System.Windows.Forms.Button();
+            this.ChooseDestination = new System.Windows.Forms.Button();
+            this.DestinationPath = new System.Windows.Forms.TextBox();
+            this.ListPhotos = new System.Windows.Forms.ListBox();
+            this.ImagePrevious = new System.Windows.Forms.PictureBox();
+            this.ImageAfter = new System.Windows.Forms.PictureBox();
+            this.SelectWatermark = new System.Windows.Forms.Button();
+            this.WatermarkPath = new System.Windows.Forms.TextBox();
+            this.ApplyWatermark = new System.Windows.Forms.CheckBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.WatermarkPreview = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePrevious)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageAfter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WatermarkPreview)).BeginInit();
             this.SuspendLayout();
+            // 
+            // SourcePath
+            // 
+            this.SourcePath.Location = new System.Drawing.Point(188, 23);
+            this.SourcePath.Name = "SourcePath";
+            this.SourcePath.Size = new System.Drawing.Size(500, 22);
+            this.SourcePath.TabIndex = 0;
+            // 
+            // SelectImages
+            // 
+            this.SelectImages.Location = new System.Drawing.Point(32, 23);
+            this.SelectImages.Name = "SelectImages";
+            this.SelectImages.Size = new System.Drawing.Size(132, 23);
+            this.SelectImages.TabIndex = 1;
+            this.SelectImages.Text = "Select Images";
+            this.SelectImages.UseVisualStyleBackColor = true;
+            this.SelectImages.Click += new System.EventHandler(this.SelectImages_Click);
+            // 
+            // ChooseDestination
+            // 
+            this.ChooseDestination.Location = new System.Drawing.Point(733, 24);
+            this.ChooseDestination.Name = "ChooseDestination";
+            this.ChooseDestination.Size = new System.Drawing.Size(159, 23);
+            this.ChooseDestination.TabIndex = 2;
+            this.ChooseDestination.Text = "Choose Destination";
+            this.ChooseDestination.UseVisualStyleBackColor = true;
+            // 
+            // DestinationPath
+            // 
+            this.DestinationPath.Location = new System.Drawing.Point(908, 24);
+            this.DestinationPath.Name = "DestinationPath";
+            this.DestinationPath.Size = new System.Drawing.Size(500, 22);
+            this.DestinationPath.TabIndex = 3;
+            // 
+            // ListPhotos
+            // 
+            this.ListPhotos.FormattingEnabled = true;
+            this.ListPhotos.ItemHeight = 16;
+            this.ListPhotos.Location = new System.Drawing.Point(32, 89);
+            this.ListPhotos.Name = "ListPhotos";
+            this.ListPhotos.Size = new System.Drawing.Size(315, 932);
+            this.ListPhotos.TabIndex = 4;
+            // 
+            // ImagePrevious
+            // 
+            this.ImagePrevious.Location = new System.Drawing.Point(385, 89);
+            this.ImagePrevious.Name = "ImagePrevious";
+            this.ImagePrevious.Size = new System.Drawing.Size(700, 700);
+            this.ImagePrevious.TabIndex = 5;
+            this.ImagePrevious.TabStop = false;
+            // 
+            // ImageAfter
+            // 
+            this.ImageAfter.Location = new System.Drawing.Point(1139, 89);
+            this.ImageAfter.Name = "ImageAfter";
+            this.ImageAfter.Size = new System.Drawing.Size(700, 700);
+            this.ImageAfter.TabIndex = 6;
+            this.ImageAfter.TabStop = false;
+            // 
+            // SelectWatermark
+            // 
+            this.SelectWatermark.Location = new System.Drawing.Point(1146, 823);
+            this.SelectWatermark.Name = "SelectWatermark";
+            this.SelectWatermark.Size = new System.Drawing.Size(140, 23);
+            this.SelectWatermark.TabIndex = 7;
+            this.SelectWatermark.Text = "Select Watermark";
+            this.SelectWatermark.UseVisualStyleBackColor = true;
+            this.SelectWatermark.Click += new System.EventHandler(this.SelectWatermark_Click);
+            // 
+            // WatermarkPath
+            // 
+            this.WatermarkPath.Location = new System.Drawing.Point(1292, 823);
+            this.WatermarkPath.Name = "WatermarkPath";
+            this.WatermarkPath.Size = new System.Drawing.Size(547, 22);
+            this.WatermarkPath.TabIndex = 8;
+            // 
+            // ApplyWatermark
+            // 
+            this.ApplyWatermark.AutoSize = true;
+            this.ApplyWatermark.Location = new System.Drawing.Point(1153, 861);
+            this.ApplyWatermark.Name = "ApplyWatermark";
+            this.ApplyWatermark.Size = new System.Drawing.Size(133, 20);
+            this.ApplyWatermark.TabIndex = 9;
+            this.ApplyWatermark.Text = "Apply Watermark";
+            this.ApplyWatermark.UseVisualStyleBackColor = true;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(1636, 904);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(203, 72);
+            this.Start.TabIndex = 10;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // WatermarkPreview
+            // 
+            this.WatermarkPreview.Location = new System.Drawing.Point(850, 850);
+            this.WatermarkPreview.Name = "WatermarkPreview";
+            this.WatermarkPreview.Size = new System.Drawing.Size(252, 180);
+            this.WatermarkPreview.TabIndex = 11;
+            this.WatermarkPreview.TabStop = false;
             // 
             // HomePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.WatermarkPreview);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.ApplyWatermark);
+            this.Controls.Add(this.WatermarkPath);
+            this.Controls.Add(this.SelectWatermark);
+            this.Controls.Add(this.ImageAfter);
+            this.Controls.Add(this.ImagePrevious);
+            this.Controls.Add(this.ListPhotos);
+            this.Controls.Add(this.DestinationPath);
+            this.Controls.Add(this.ChooseDestination);
+            this.Controls.Add(this.SelectImages);
+            this.Controls.Add(this.SourcePath);
             this.Name = "HomePage";
-            this.Text = "Photo App";
+            this.Text = "PhotoApp";
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePrevious)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageAfter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WatermarkPreview)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox SourcePath;
+        private System.Windows.Forms.Button SelectImages;
+        private System.Windows.Forms.Button ChooseDestination;
+        private System.Windows.Forms.TextBox DestinationPath;
+        private System.Windows.Forms.ListBox ListPhotos;
+        private System.Windows.Forms.PictureBox ImagePrevious;
+        private System.Windows.Forms.PictureBox ImageAfter;
+        private System.Windows.Forms.Button SelectWatermark;
+        private System.Windows.Forms.TextBox WatermarkPath;
+        private System.Windows.Forms.CheckBox ApplyWatermark;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.PictureBox WatermarkPreview;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,18 +3,151 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace photoApp
 {
     public partial class HomePage : Form
     {
+        private string WatermarkFilePath;
+        private string imgLocation;
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        private void SelectImages_Click(object sender, EventArgs e)
+        {
+            //Open up a new file explorer window to open a directory
+            FolderBrowserDialog SelectDirectory = new FolderBrowserDialog();
+            //Set the root folder
+            SelectDirectory.RootFolder = Environment.SpecialFolder.Desktop;
+            //Show the description in the explorer window
+            SelectDirectory.Description = "Select your folder";
+            //shows the new folder button
+            SelectDirectory.ShowNewFolderButton = true;
+
+            //clear the previous files in the list
+            ListPhotos.Items.Clear();
+
+
+
+            if (SelectDirectory.ShowDialog() == DialogResult.OK)
+            {
+                //set the selected directory in the textbox path
+                SourcePath.Text = SelectDirectory.SelectedPath;
+
+                //put all the files in the directory
+                DirectoryInfo dinfo = new DirectoryInfo(SelectDirectory.SelectedPath);
+
+                FileInfo[] Files = dinfo.GetFiles("*.jpg");
+
+                foreach (FileInfo file in Files)
+                {
+                    ListPhotos.Items.Add(file.Name);
+                }
+
+            }
+        }
+
+        private void SelectWatermark_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ChooseWatermark = new OpenFileDialog();
+            ChooseWatermark.Filter = "Png Files (*.png)|*.png|All Files (*.*)|*.*";
+            ChooseWatermark.FilterIndex = 1;
+            ChooseWatermark.InitialDirectory = "c:\\";
+            ChooseWatermark.RestoreDirectory = false;
+
+
+            if (ChooseWatermark.ShowDialog() == DialogResult.OK)
+            {
+                WatermarkFilePath = ChooseWatermark.FileName;
+                WatermarkPreview.SizeMode = PictureBoxSizeMode.Zoom;
+                using (Bitmap tmpBitmap = new Bitmap(WatermarkFilePath))
+                {
+                    WatermarkPreview.Image = new Bitmap(tmpBitmap);
+                }
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (ApplyWatermark.Checked)
+            {
+                string imageFolderPath = SourcePath.Text;
+                string outputFolderPath = DestinationPath.Text;
+                string watermarkImage = WatermarkFilePath;
+                ImageAfter.SizeMode = PictureBoxSizeMode.Zoom;
+
+                // Controleer of de directories bestaan
+                if (Directory.Exists(imageFolderPath) && Directory.Exists(outputFolderPath))
+                {
+                    string[] imageFiles = Directory.GetFiles(imageFolderPath, "*.*", SearchOption.TopDirectoryOnly);
+
+                    foreach (string imgLocation in imageFiles)
+                    {
+                        try
+                        {
+                            using (Image image = Image.FromFile(imgLocation))
+                            {
+                                using (Graphics g = Graphics.FromImage(image))
+                                {
+                                    Image watermark = Image.FromFile(watermarkImage);
+                                    g.DrawImage(watermark, new Point(0, 0));
+                                    string outputFileName = Path.Combine(outputFolderPath, "watermarkadded-" + Path.GetFileName(imgLocation));
+                                    image.Save(outputFileName);
+                                    ImageAfter.Image = new Bitmap(outputFileName);
+                                    ListPhotos.Items.Clear();
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error processing image: " + imgLocation + "\n" + ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please ensure the image folder and output folder exist.");
+                }
+            }
+            else
+            {
+                string imageFolderPath = SourcePath.Text;
+                string outputFolderPath = DestinationPath.Text;
+
+                // Controleer of de directories bestaan
+                if (Directory.Exists(imageFolderPath) && Directory.Exists(outputFolderPath))
+                {
+                    string[] imageFiles = Directory.GetFiles(imageFolderPath, "*.*", SearchOption.TopDirectoryOnly);
+
+                    foreach (string imgLocation in imageFiles)
+                    {
+                        try
+                        {
+                            string destinationFileName = Path.Combine(outputFolderPath, Path.GetFileName(imgLocation));
+                            File.Move(imgLocation, destinationFileName);
+                            ListPhotos.Items.Clear();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error moving image: " + imgLocation + "\n" + ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please ensure the image folder and output folder exist.");
+                }
+            }
         }
     }
 }
